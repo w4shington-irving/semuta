edcaa01f72a173a2fdf612337549786ad1ff9d96
w4shington-irving <maciejmajchrowicz@icloud.com>
2025-12-16 16:52:17 +0100

    modified:   src/ui/display.rs

--- src/ui/display.rs
+++ src/ui/display.rs
@@ -2,9 +2,9 @@ use crate::db;
 
 pub fn print_library() {
     for artist in db::get_artists().expect("Failed to get artists") {
-        println!("Artist: {}", artist.name);
+        println!("* {}", artist.name);
         for album in db::get_albums_by_artist_id(artist.id).expect("Failed to get albums") {
-            println!("  Album: {} ID: {}", album.title, album.id);
+            println!("  - {}", album.title);
             for track in db::get_tracks_by_album_id(album.id).expect("Failed to get tracks") {
                 println!("      [{}] - {} ({}m{}s)", 
                     track.track_number.unwrap_or(0), 
